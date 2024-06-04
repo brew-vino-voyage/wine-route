6b8709ec380bb4b7f6e093724a88e6967f1f7dbe
상은 <71699054+vmkmym@users.noreply.github.com>
2024-06-04 23:06:44 +0900

    Initial commit

--- app/src/main/java/com/brew/wine_route/MainActivity.kt
+++ app/src/main/java/com/brew/wine_route/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             WineRouteTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Android!",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
