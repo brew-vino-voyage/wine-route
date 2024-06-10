a12c42daf7cddbdbbd9388019c7ebaf3730ea620
bbangiee <yoojin1117@gmail.com>
2024-06-10 16:55:54 +0900

    feat: Add TextFieldLine function
    - Add IconButton next to TextField
    - Refactoring LoginTextField
    
    Fixes: #7

--- app/src/main/java/com/brew/wine_route/screen/LoginScreen.kt
+++ app/src/main/java/com/brew/wine_route/screen/LoginScreen.kt
@@ -1,12 +1,15 @@
 package com.brew.wine_route.screen
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -15,8 +18,12 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import com.brew.wine_route.R
+import org.checkerframework.checker.units.qual.C
 
 @Preview(showBackground = true)
 @Composable
@@ -31,16 +38,19 @@ fun LoginScreen() {
         Row(
             modifier = Modifier.fillMaxWidth()
         ) {
-            ChoiceButton(text = "로그인", position = 0.5f)
-            ChoiceButton(text = "회원가입", position = 1f)
+            SignChoiceButton(text = "로그인", position = 0.5f)
+            SignChoiceButton(text = "회원가입", position = 1f)
         }
-        LoginTextField(
+        TextFieldLine(
             value = email,
             onValueChange = { email = it },
             label = {
                 Text(text = "이메일을 입력하세요")
-            }
-        )
+            },
+            imageVector = com.facebook.login.R.drawable.com_facebook_close
+        ) {
+            // onClick for IconButton
+        }
         LoginTextField(
             value = password,
             onValueChange = { password = it },
@@ -52,7 +62,7 @@ fun LoginScreen() {
 }
 
 @Composable
-fun ChoiceButton(text: String, position: Float) {
+fun SignChoiceButton(text: String, position: Float) {
     Button(
         onClick = {  },
         modifier = Modifier
@@ -76,4 +86,24 @@ fun LoginTextField(
         label = label,
         keyboardOptions = if (value == "password") KeyboardOptions(keyboardType = KeyboardType.NumberPassword) else KeyboardOptions.Default
     )
+}
+
+@Composable
+fun TextFieldLine(
+    value: String,
+    onValueChange: (String) -> Unit,
+    label: @Composable () -> Unit,
+    imageVector: Int,
+    onClick: () -> Unit
+) {
+    Box(modifier = Modifier.fillMaxWidth()) {
+        LoginTextField(
+            value = value,
+            onValueChange = onValueChange,
+            label = label
+        )
+        IconButton(onClick = onClick) {
+            Icon(imageVector = ImageVector.vectorResource(id = imageVector), contentDescription = "null")
+        }
+    }
 }
