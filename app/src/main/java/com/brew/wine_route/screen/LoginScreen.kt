e2c8d075221b032a7a36b667711d00103c3407bc
bbangiee <yoojin1117@gmail.com>
2024-06-14 00:00:50 +0900

    feat: Finish login page
    - Add Button for login
    - Add Button for find id or password
    - Add Text for separate
    - Add Buttons for social login
    
    Fixes: #7

--- app/src/main/java/com/brew/wine_route/screen/LoginScreen.kt
+++ app/src/main/java/com/brew/wine_route/screen/LoginScreen.kt
@@ -1,5 +1,7 @@
 package com.brew.wine_route.screen
 
+import android.widget.Button
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -13,8 +15,11 @@ import androidx.compose.material.icons.filled.Call
 import androidx.compose.material.icons.rounded.Add
 import androidx.compose.material.icons.rounded.Call
 import androidx.compose.material.icons.rounded.Close
+import androidx.compose.material.icons.rounded.DesktopMac
 import androidx.compose.material.icons.rounded.DiscFull
+import androidx.compose.material.icons.rounded.Laptop
 import androidx.compose.material.icons.rounded.List
+import androidx.compose.material.icons.rounded.TabletAndroid
 import androidx.compose.material.icons.rounded.Visibility
 import androidx.compose.material.icons.rounded.VisibilityOff
 import androidx.compose.material3.Button
@@ -33,6 +38,7 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 
 @Preview(showBackground = true)
@@ -49,8 +55,18 @@ fun LoginScreen() {
         Row(
             modifier = Modifier.fillMaxWidth()
         ) {
-            SignChoiceButton(text = "로그인", position = 0.5f)
-            SignChoiceButton(text = "회원가입", position = 1f)
+            SignChoiceButton(
+                text = "로그인",
+                position = 0.5f
+            ) {
+                // click login button
+            }
+            SignChoiceButton(
+                text = "회원가입",
+                position = 1f
+            ) {
+                // click sign up button
+            }
         }
         LoginTextField(
             value = email,
@@ -79,13 +95,45 @@ fun LoginScreen() {
         ) {
             hidePassword = !hidePassword
         }
+        GoNextButton(
+            text = "로그인"
+        ) {
+            // click login button
+        }
+        Button(
+            onClick = {
+                // click forget password
+            },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "비밀번호를 잊으셨나요?")
+        }
+        Text(
+            text = "또는",
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Row(
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            SocialLoginButton(imageVector = Icons.Rounded.TabletAndroid) {
+                
+            }
+            SocialLoginButton(imageVector = Icons.Rounded.Laptop) {
+                
+            }
+            SocialLoginButton(imageVector = Icons.Rounded.DesktopMac) {
+                
+            }
+        }
     }
 }
 
 @Composable
-fun SignChoiceButton(text: String, position: Float) {
+fun SignChoiceButton(text: String, position: Float, onClick: () -> Unit) {
     Button(
-        onClick = { },
+        onClick = onClick,
         modifier = Modifier
             .fillMaxWidth(position)
     ) {
@@ -121,4 +169,21 @@ fun LoginTextField(
         },
         modifier = modifier.fillMaxWidth()
     )
+}
+
+@Composable
+fun GoNextButton(text: String, onClick: () -> Unit) {
+    Button(
+        onClick = onClick,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Text(text = text, textAlign = TextAlign.Center, modifier = Modifier.fillMaxWidth())
+    }
+}
+
+@Composable
+fun SocialLoginButton(imageVector: ImageVector, onClick: () -> Unit) {
+    IconButton(onClick = onClick) {
+        Icon(imageVector = imageVector, contentDescription = null)
+    }
 }
