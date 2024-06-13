0fb48ee96f7b8c135b8a72798d7da39050922eee
bbangiee <yoojin1117@gmail.com>
2024-06-13 23:31:07 +0900

    feat: Finish LoginTextField
    - Remove LoginTextFieldLine()
    - Add function which can hide password and when click IconButton checking password can be possible
    
    Fixes: #7

--- app/src/main/java/com/brew/wine_route/screen/LoginScreen.kt
+++ app/src/main/java/com/brew/wine_route/screen/LoginScreen.kt
@@ -7,6 +7,16 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.Call
+import androidx.compose.material.icons.rounded.Add
+import androidx.compose.material.icons.rounded.Call
+import androidx.compose.material.icons.rounded.Close
+import androidx.compose.material.icons.rounded.DiscFull
+import androidx.compose.material.icons.rounded.List
+import androidx.compose.material.icons.rounded.Visibility
+import androidx.compose.material.icons.rounded.VisibilityOff
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -17,19 +27,20 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
-import com.brew.wine_route.R
-import org.checkerframework.checker.units.qual.C
 
 @Preview(showBackground = true)
 @Composable
 fun LoginScreen() {
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
+    var hidePassword by remember { mutableStateOf(true) }
 
     Column(
         verticalArrangement = Arrangement.Center,
@@ -41,30 +52,34 @@ fun LoginScreen() {
             SignChoiceButton(text = "로그인", position = 0.5f)
             SignChoiceButton(text = "회원가입", position = 1f)
         }
-        TextFieldLine(
+        LoginTextField(
             value = email,
             onValueChange = { email = it },
             label = {
                 Text(text = "이메일을 입력하세요")
             },
-            imageVector = com.facebook.login.R.drawable.com_facebook_close
+            imageVector = Icons.Rounded.Close
         ) {
-            // onClick for IconButton
+            email = ""
         }
         LoginTextField(
             value = password,
             onValueChange = { password = it },
             label = {
                 Text(text = "**********")
-            }
-        )
+            },
+            hidePassword = hidePassword,
+            imageVector = if (hidePassword) Icons.Rounded.Visibility else Icons.Rounded.VisibilityOff
+        ) {
+            hidePassword = !hidePassword
+        }
     }
 }
 
 @Composable
 fun SignChoiceButton(text: String, position: Float) {
     Button(
-        onClick = {  },
+        onClick = { },
         modifier = Modifier
             .fillMaxWidth(position)
     ) {
@@ -79,31 +94,23 @@ fun LoginTextField(
     value: String,
     onValueChange: (String) -> Unit,
     label: @Composable () -> Unit,
+    modifier: Modifier = Modifier,
+    hidePassword: Boolean = false,
+    imageVector: ImageVector,
+    onClick: () -> Unit,
 ) {
     OutlinedTextField(
         value = value,
         onValueChange = onValueChange,
         label = label,
-        keyboardOptions = if (value == "password") KeyboardOptions(keyboardType = KeyboardType.NumberPassword) else KeyboardOptions.Default
+        keyboardOptions = if (value == "password" && hidePassword) KeyboardOptions(keyboardType = KeyboardType.Password) else KeyboardOptions.Default,
+        visualTransformation = if (hidePassword) PasswordVisualTransformation() else VisualTransformation.None,
+        maxLines = 1,
+        trailingIcon = {
+            IconButton(onClick = onClick) {
+                Icon(imageVector = imageVector, contentDescription = "null")
+            }
+        },
+        modifier = modifier.fillMaxWidth()
     )
-}
-
-@Composable
-fun TextFieldLine(
-    value: String,
-    onValueChange: (String) -> Unit,
-    label: @Composable () -> Unit,
-    imageVector: Int,
-    onClick: () -> Unit
-) {
-    Box(modifier = Modifier.fillMaxWidth()) {
-        LoginTextField(
-            value = value,
-            onValueChange = onValueChange,
-            label = label
-        )
-        IconButton(onClick = onClick) {
-            Icon(imageVector = ImageVector.vectorResource(id = imageVector), contentDescription = "null")
-        }
-    }
 }
