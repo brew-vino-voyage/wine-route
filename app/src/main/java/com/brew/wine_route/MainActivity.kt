4c7ead642b87710152f5a8a096217f3650790798
bbangiee <yoojin1117@gmail.com>
2024-06-13 23:28:04 +0900

    test: No change
    - Edit for emulator but fix bug
    - Error of Facebook Icon
    - Use Material Icon rather than Icon which make error
    
    Fixes: #7

--- app/src/main/java/com/brew/wine_route/MainActivity.kt
+++ app/src/main/java/com/brew/wine_route/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.brew.wine_route.screen.LoginScreen
 import com.brew.wine_route.ui.theme.WineRouteTheme
 
 class MainActivity : ComponentActivity() {
