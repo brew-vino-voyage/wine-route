99b31de4cd01dcf12ec4811816f595d7f345e782
상은 <71699054+vmkmym@users.noreply.github.com>
2024-06-15 15:03:51 +0900

    feat: 이메일, 비밀번호 로그인 로직 구현
    related #4

--- app/src/main/java/com/brew/wine_route/model/signInHandler/EmailPasswordActivity.kt
+++ app/src/main/java/com/brew/wine_route/model/signInHandler/EmailPasswordActivity.kt
@@ -1,4 +1,4 @@
-package com.brew.wine_route.model.firebaseAuth
+package com.brew.wine_route.model.signInHandler
 
 import android.os.Bundle
 import android.util.Log
@@ -20,9 +20,7 @@ class EmailPasswordActivity : ComponentActivity() {
     public override fun onStart() {
         super.onStart()
         val currentUser = auth.currentUser
-        if (currentUser != null) {
-            reload()
-        }
+        if (currentUser != null) { reload() }
         updateUI(currentUser)
     }
 
@@ -30,18 +28,12 @@ class EmailPasswordActivity : ComponentActivity() {
         auth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
-                    // Sign in success, update UI with the signed-in user's information
                     Log.d(TAG, "createUserWithEmail:success")
                     val user = auth.currentUser
                     updateUI(user)
                 } else {
-                    // If sign in fails, display a message to the user.
                     Log.w(TAG, "createUserWithEmail:failure", task.exception)
-                    Toast.makeText(
-                        baseContext,
-                        "Authentication failed.",
-                        Toast.LENGTH_SHORT,
-                    ).show()
+                    Toast.makeText(baseContext, "Authentication failed.", Toast.LENGTH_SHORT,).show()
                     updateUI(null)
                 }
             }
@@ -51,18 +43,12 @@ class EmailPasswordActivity : ComponentActivity() {
         auth.signInWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
-                    // Sign in success, update UI with the signed-in user's information
                     Log.d(TAG, "signInWithEmail:success")
                     val user = auth.currentUser
                     updateUI(user)
                 } else {
-                    // If sign in fails, display a message to the user.
                     Log.w(TAG, "signInWithEmail:failure", task.exception)
-                    Toast.makeText(
-                        baseContext,
-                        "Authentication failed.",
-                        Toast.LENGTH_SHORT,
-                    ).show()
+                    Toast.makeText(baseContext, "Authentication failed.", Toast.LENGTH_SHORT,).show()
                     updateUI(null)
                 }
             }
@@ -73,6 +59,20 @@ class EmailPasswordActivity : ComponentActivity() {
     }
 
     private fun updateUI(user: FirebaseUser?) {
+        user?.let {
+            // Name, email address, and profile photo Url
+            val name = it.displayName
+            val email = it.email
+            val photoUrl = it.photoUrl
+
+            // Check if user's email is verified
+            val emailVerified = it.isEmailVerified
+
+            // The user's ID, unique to the Firebase project. Do NOT use this value to
+            // authenticate with your backend server, if you have one. Use
+            // FirebaseUser.getIdToken() instead.
+            val uid = it.uid
+        }
     }
 
     private fun reload() {
