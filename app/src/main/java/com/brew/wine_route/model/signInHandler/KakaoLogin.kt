ccd7875fbfe5622166907e92b6d733241e929cc5
상은 <71699054+vmkmym@users.noreply.github.com>
2024-06-27 23:01:41 +0900

    style: kakao sdk key 수정

--- app/src/main/java/com/brew/wine_route/model/signInHandler/KakaoLogin.kt
+++ app/src/main/java/com/brew/wine_route/model/signInHandler/KakaoLogin.kt
@@ -8,6 +8,6 @@ import com.kakao.sdk.common.KakaoSdk
 class KakaoLogin : Application() {
     override fun onCreate() {
         super.onCreate()
-        KakaoSdk.init(this, "4e66d0e4c6f06cbd956a6d246b00c9a5")
+        KakaoSdk.init(this, "@string/kakao_app_native_key")
     }
 }
